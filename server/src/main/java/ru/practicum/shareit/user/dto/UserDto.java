727f74a0b4e843e8895a87dd4f56f704afdb0b72
Nov-net <ek9062625020@gmail.com>
2023-01-26 17:45:01 +0300

    feat: docker, gateway, server

--- server/src/main/java/ru/practicum/shareit/user/dto/UserDto.java
+++ server/src/main/java/ru/practicum/shareit/user/dto/UserDto.java
@@ -1,14 +1,11 @@
 package ru.practicum.shareit.user.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.RequiredArgsConstructor;
+import lombok.*;
 
 @Data
 @Builder
 @AllArgsConstructor
-@RequiredArgsConstructor
+@NoArgsConstructor
 public class UserDto {
     private Long id;
     private String name;
